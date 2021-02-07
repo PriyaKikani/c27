af8ae706be9dc7a9e816a92b90307f39ddd25d65
PriyaKikani <75695650+PriyaKikani@users.noreply.github.com>
2021-02-07 23:17:51 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+The Newton's Cradle is a toy which demonstrates transfer of momentum when two objects or a group of objects collide.
+Press up arrow key.
+https://madhura247.github.io/project27/
---
+++ bob.js
@@ -0,0 +1,40 @@
+class Bob {
+   constructor(x, y,r) {
+      var options={
+         isStatic : false,
+         restitution: 1,
+         friction : 0,
+         density : 0.8,
+      }
+
+       this.x = x;
+       this.y = y;
+       this.r = r;
+
+       this.body = Bodies.circle(x, y, r, options);
+       
+       World.add(world, this.body);
+   }
+
+
+   display() {
+   
+   var pos = this.body.position;
+
+   push();
+   translate(pos.x, pos.y)
+   ellipseMode(RADIUS);
+   fill ("maroon")
+   stroke("black");
+   ellipse(0, 0,this.r,this.r);
+   pop();
+   }
+
+   }
+
+
+
+
+
+           
+        
